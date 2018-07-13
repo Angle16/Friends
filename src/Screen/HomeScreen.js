665807cfc5ad7ec3f6e260a67365a8a602febba5
Your Name <you@example.com>
2018-07-13 16:02:40 +0800

    homeScreen1

---
+++ src/Screen/HomeScreen.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import
+{
+    Toast,
+    NavBar,
+    ListView,
+    PullToRefresh,
+  
+} from 'antd-mobile';
+import accountManager from '../DataServer/AccountManager';
+import messageManager from "../DataSever/MessageManager";
+import FriendListItem from '../ViewComponent/FriendListItem';
+
