0f95747e5fe605c3b72f247ee31bd133dce37fb8
Your Name <you@example.com>
2018-07-13 10:22:18 +0800

    yici

---
+++ src/Screen/LoginScreen.js
@@ -0,0 +1,74 @@
+import React,{Component} from 'react';
+import {
+Button,
+Toast,
+WingBlank,
+List,
+InputItem,
+Flex
+} from 'antd-mobile';
+import accountManager from '../DataServer/AccountManager';
+export default class LoginScreen extends Component{
+    componentDidCatch(){
+        if(accountManager.isLogin()===true){
+            this.props.history.replacr('/TabBarScreen');
+        }
+    }
+    constructor(props){
+        super(props)
+        this.state={
+            email:'',
+            password:''
+        }
+    }
+    render(){
+        return(
+            <div>
+                <img
+                alt=''
+                src={require('../images/logo.jpg')}
+                style={{width:'100%'}}
+                />
+                <List>
+                    <InputItem
+                    type={'text'}
+                    value={this.state.email}
+                    onChange={(email)=>{this.setState({email})}}
+                    placeholder={'请输入登录账号'}
+                    >邮箱
+                    </InputItem>
+                    <InputItem
+                    type={'password'}
+                    value={this.state.password}
+                    onChange={(password)=>{this.setState({password})}}
+                    placeholder={'请输入登录密码'}
+                    >密码
+                    </InputItem>
+                    </List>
+                    <whiteSpcce/>
+                    <WingBlank>
+                        <Flex>
+                            <Button
+                            style={{flex:1,marginRight:5}}
+                            type={'primary'}
+                            onClick={async()=>{
+                                const result=await accountManager.login(this.state.email,this.state.password);
+                                console.log(result);
+                                if(result.success===false){
+                                    Toast.fail(result.erroeMessage,1);
+                                    return;
+                                }
+                                if(!result.data.userId){
+                                    this.props.history.push('/CreateUserScreen');
+                                    return;
+                                }
+                                this.props.history.replace('/TabBarScreen');
+                            }}
+                            >登录
+                                </Button>
+                            </Flex>
+                            </WingBlank>
+                </div>
+        )
+    }
+}
