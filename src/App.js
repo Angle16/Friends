03ae53810b5331444e201fdf550b6fdc61fc3052
Your Name <you@example.com>
2018-07-18 10:40:29 +0800

    app

--- src/App.js
+++ src/App.js
@@ -10,26 +10,32 @@ import {
 
 import { Button } from 'antd-mobile';
 
-import AScreen from './Screen/AScreen';
-import BScreen from './Screen/BScreen';
 
+import LoginScreen from './Screen/LoginScreen';
+import RegisterScreen from './Screen/RegisterScreen';
+import CreateUserScreen from './Screen/CreateUserScreen';
+import TabBarScreen from './Screen/TabBarScreen';
+import CreateMessageScreen from './Screen/CreateMessageScreen';
+import AddFriendScreen from './Screen/AddFriendScreen';
+import UserScreen from './Screen/UserScreen';
+import ChangePassWordScreen from './Screen/ChangePassWordScreen';
+import ChangePersonInfoScreen from './Screen/ChangePersonInfoScreen';
 class App extends Component {
   render() {
     return (
-      <DocumentTitle title='HomeTitle'>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Button>Click</Button>
-          <Route path={'/a'} component={AScreen} />
-          <Route path={'/b'} component={BScreen} />
+     
+        <div>
+         <Route exact path='/' component={LoginScreen} />
+        <Route path={'/RegisterScreen'} component={RegisterScreen} />
+        <Route path={'/CreateUserScreen'} component={CreateUserScreen} />
+        <Route path={'/TabBarScreen'} component={TabBarScreen} />
+        <Route path={'/CreateMessageScreen'} component={CreateMessageScreen} />
+        <Route path={'/AddFriendScreen'} component={AddFriendScreen} />
+        <Route path={'/UserScreen'} component={UserScreen} />
+        <Route path={'/ChangePassWordScreen'} component={ChangePassWordScreen} />
+        <Route path={'/ChangePersonInfoScreen'} component={ChangePersonInfoScreen} />
         </div>
-      </DocumentTitle>
+     
     );
   }
 }
